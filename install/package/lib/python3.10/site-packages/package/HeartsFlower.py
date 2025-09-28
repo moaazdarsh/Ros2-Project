a17302e67191d70675c9663f70a0f612ea852550
moaazdarsh <mo3az.misara@gmail.com>
2025-09-29 00:12:20 +0300

    Video

---
+++ install/package/lib/python3.10/site-packages/package/HeartsFlower.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from turtlesim.msg import Pose
+from geometry_msgs.msg import Twist
+from time import sleep
+
+class Mover(Node): 
+    def __init__(self): 
+        super().__init__('Mover')
+        self.publisher = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
+        self.timer = self.create_timer(7, self.draw) 
+
+    def draw(self):
+        msg = Twist()
+        shape = [(0, 0.75), (1.8, 0), (2, 3.14 + 1.507-0.75), (0, 3.14), (2, 3.14 + 1.507-0.75), (1.8, 0), (0, 0)]
+        for linear, angular in shape:
+            msg.linear.x = float(linear)
+            msg.angular.z = float(angular)
+            self.publisher.publish(msg)
+            sleep(1)
+        
+
+rclpy.init(args=None)
+node = Mover()
+node.draw()
+rclpy.spin(node) 
+node.destroy_node() 
+rclpy.shutdown()
